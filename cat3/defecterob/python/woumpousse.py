5659c073acf9603bb66ecb3e63ea2aeee74c5a02
Frederic Vogels <frederic.vogels@gmail.com>
2018-03-24 21:09:52 +0100

    defecterobot/python

--- cat3/defecterob/python/woumpousse.py
+++ cat3/defecterob/python/woumpousse.py
@@ -1,11 +1,12 @@
 alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-
 inverse_alphabet = { alphabet[i] : i for i in range(len(alphabet)) }
 
-def pairs(xs):
-    return [ (xs[2*i], xs[2*i+1]) for i in range(len(xs) // 2) ]
 
 class InfiniteString:
+    '''
+    Infinitely long string. Used to represent 'no solution possible'.
+    '''
+    
     def __len__(self):
         return 99999
 
@@ -15,16 +16,18 @@ class InfiniteString:
     def __str__(self):
         return "onmogelijk"
 
-
+    
 def first(predicate, xs, default):
     for x in xs:
         if predicate(x):
             return x
 
     return default
-    
+
+
 def common_prefix_length(s1, s2):
     return first(lambda i: s1[i] != s2[i], range(0, len(s1)), len(s1))
+
 
 class Triplet:
     def __init__(self, s1, s2, delta):
@@ -33,12 +36,8 @@ class Triplet:
         self.s1 = s1[prefix_length:]
         self.s2 = s2[prefix_length:]
         self.delta = delta
+        self.score = 'Z' * sum(1 for i in range(len(self.s1)) if self.s1[i] != self.s2[i]) + delta
 
-    def heuristic(self):
-        return sum(1 for i in range(len(self.s1)) if self.s1[i] != self.s2[i])
-
-    def score(self):
-        return len(self.delta) + self.heuristic()
     
 def swap(s, i, j):
     cs = list(s)
@@ -49,8 +48,10 @@ def swap(s, i, j):
 
     return "".join(cs)
 
+
 def code(i, j):
     return alphabet[i] + alphabet[j]
+
 
 def shortest(strings):
     strings = strings + [ InfiniteString() ]
@@ -58,7 +59,10 @@ def shortest(strings):
     smallest_strings = [ s for s in strings if len(s) == smallest_length ]
     return sorted(smallest_strings)[0]
 
+
 def concat(s1, s2):
+    '''String concatenation, but with support for InfiniteStrings'''
+    
     if type(s1) is InfiniteString or type(s2) is InfiniteString:
         return InfiniteString()
     else:
@@ -80,7 +84,7 @@ def solve(string1, string2):
                 delta = current.delta + code(len(string1) - len(s1), len(string1) - len(s1) + i)
                 queue.append(Triplet(s1, s2, delta))
 
-            queue.sort(key=lambda x: x.score(), reverse=True)
+            queue.sort(key=lambda x: x.score, reverse=True)
 
     return 'onmogelijk'
 
@@ -94,6 +98,9 @@ def possible(s1, s2):
 
     return l1 == l2 and r1 == r2 and s1 == s2
 
+
+def pairs(xs):
+    return [ (xs[2*i], xs[2*i+1]) for i in range(len(xs) // 2) ]
 
 def verify(s1, s2, delta):
     for (i, j) in [ (inverse_alphabet[x], inverse_alphabet[y]) for (x, y) in pairs(delta) ]:
