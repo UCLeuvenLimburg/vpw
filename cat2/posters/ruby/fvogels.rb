fcbf9d014bf7a6a25da9f46a13786833e60973b1
Frederic Vogels <frederic.vogels@ucll.be>
2019-01-30 18:00:17 +0100

    cat2/posters/ruby

--- cat2/posters/ruby/fvogels.rb
+++ cat2/posters/ruby/fvogels.rb
@@ -1,25 +1,19 @@
 (1..gets.to_i).each do |index|
-  posters = (1..gets.to_i).map.with_index do |i, j|
-    [j] + gets.split(/ /).map(&:to_i)
+  posters = (1..gets.to_i).map do
+    gets.split(/ /).map(&:to_i)
   end
 
-  wall_size = posters.map { |index, start, length| start + length }.max
+  wall_size = posters.map { |start, length| start + length }.max
 
-  wall = [nil] * wall_size
+  wall = [nil] * (wall_size + 1)
 
-  posters.each do |index, start, length|
+  posters.each.with_index do |poster, index|
+    start, length = poster
+
     (start...start+length).each do |i|
       wall[i] = index
     end
   end
 
-  set = Hash.new false
-
-  wall.each do |index|
-    if index then
-      set[index] = true
-    end
-  end
-
-  puts set.size
+  puts(wall.uniq.size - 1)
 end
