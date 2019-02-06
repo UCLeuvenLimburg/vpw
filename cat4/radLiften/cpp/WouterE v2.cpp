9595cd07984926a81784a50f1538e8a06df17e1c
Wouter Engelen <wouter.engelen@student.ucll.be>
2019-02-06 09:07:47 +0100

    cat4/radliften/cpp v2 updated

--- cat4/radLiften/cpp/WouterE v2.cpp
+++ cat4/radLiften/cpp/WouterE v2.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 #include <vector>
 #include <queue>
-#include <map>
 
 using namespace std;
 
@@ -16,8 +15,8 @@ bool operator > (const path& l, const path& r) {
 	return l.verdiep > r.verdiep;
 }
 
-path solve(map<int, vector<lift>> liften, int start, int target) {		// Dijkstra
-	map<int, path> minimum_paths;
+path solve(vector<vector<lift>> liften, int start, int target) {		// Dijkstra
+	vector<bool> seen(liften.size(), false);
 	priority_queue<path, vector<path>, greater<path>> open;
 	open.push(path{ 0, vi{-1}, vi{start} });
 
@@ -25,15 +24,15 @@ path solve(map<int, vector<lift>> liften, int start, int target) {		// Dijkstra
 		path p = open.top();
 		open.pop();
 
-		for (lift l : liften.at(p.verdiep.back())) {
-			path tmp = p;
-			tmp.cost += l.cost;
-			tmp.koker.push_back(l.koker);
-			tmp.verdiep.push_back(l.verdiep);
+		if (!seen.at(p.verdiep.back())) {
+			seen.at(p.verdiep.back()) = true;
 
-			if (minimum_paths.find(l.verdiep) == minimum_paths.end() || minimum_paths.at(l.verdiep) > tmp)
-			{
-				minimum_paths[l.verdiep] = tmp;
+			for (lift l : liften.at(p.verdiep.back())) {
+				path tmp = p;
+				tmp.cost += l.cost;
+				tmp.koker.push_back(l.koker);
+				tmp.verdiep.push_back(l.verdiep);
+
 				open.push(tmp);
 			}
 		}
@@ -50,15 +49,15 @@ int main() {
 		int vs, ls;
 		cin >> vs >> ls;
 
-		map<int, vector<lift>> liften;
+		vector<vector<lift>> liften(vs, vector<lift>());
 		for (int j = 0; j < ls; j++) {
 			int s, e, d;
 			cin >> s >> e >> d;
 
 			for (int from = s; from <= e; from += d) {
 				for (int to = s; to <= e; to += d) {
-					if (from < to) liften[from].push_back(lift{ (to - from) / d, 2 * j + 1, to });
-					if (from > to) liften[from].push_back(lift{ (from - to) / d, 2 * j + 2, to });
+					if (from < to) liften.at(from).push_back(lift{ (to - from) / d, 2 * j + 1, to });
+					if (from > to) liften.at(from).push_back(lift{ (from - to) / d, 2 * j + 2, to });
 				}
 			}
 		}
